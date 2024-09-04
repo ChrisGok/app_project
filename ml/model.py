fc5e707b0456c719b5340a147b038716654e3deb
Chris Gokus <lightheaded@gmx.de>
2024-09-04 22:25:49 +0200

    changed model and extended scripts

--- ml/model.py
+++ ml/model.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import AdaBoostClassifier
 
 
 # Optional: implement hyperparameter tuning.
@@ -18,7 +19,12 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    logit = AdaBoostClassifier()
+    model = logit.fit(X_train, y_train)
+
+    return model
+
+
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +63,6 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    preds = model.predict(X)
+
+    return preds
--- train_model.py
+++ train_model.py
@@ -3,8 +3,15 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+import os
+import pandas as pd
+import pickle
+from ml.data import process_data
+from ml.model import train_model
 
 # Add code to load in the data.
+
+data = pd.read_csv(os.path.join(os.getcwd(),'data_clean','census_clean.csv'))
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -19,10 +26,20 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
 # Proces the test data with the process_data function.
 
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
+
 # Train and save a model.
+
+model = train_model(X_train, y_train)
+
+filehandler = open('./model/classifier.pkl', 'wb') 
+pickle.dump(model, filehandler)
